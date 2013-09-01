acf7bd13be5e75a378ef17917aff803d855ee0a3
Samoth <samoth93@gmail.com>
2013-09-02 01:24:35 +0200

    Add groupsManagement in blocks Center, Counter, Event + Fix php errors

--- Includes/blocks/block_center.php
+++ Includes/blocks/block_center.php
@@ -1,175 +1,137 @@
 <?php
-// -------------------------------------------------------------------------//
-// Nuked-KlaN - PHP Portal                                                  //
-// http://www.nuked-klan.org                                                //
-// -------------------------------------------------------------------------//
-// This program is free software. you can redistribute it and/or modify     //
-// it under the terms of the GNU General Public License as published by     //
-// the Free Software Foundation; either version 2 of the License.           //
-// -------------------------------------------------------------------------//
-defined('INDEX_CHECK') or die ('You can\'t run this file alone.');
+/**
+ * block_center.php
+ *
+ * Display and Admin of block center
+ *
+ * @version 1.8
+ * @link http://www.nuked-klan.org Clan Management System for Gamers
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @copyright 2001-2013 Nuked-Klan (Registred Trademark)
+ */
 
-function affich_block_center($blok){
+defined('INDEX_CHECK') or die ("You can't run this file alone.");
 
-    $mod = explode('|', $blok['content']);
-    $mod1 = $mod[0];
-    $mod2 = $mod[1];
+function affich_block_center($block){
 
-    if ($mod1 == 'Gallery') $modname1 = _NAVGALLERY;
-    else if ($mod1 == 'Calendar') $modname1 = _NEXTWAR;
-    else if ($mod1 == 'Download') $modname1 = _NAVDOWNLOAD;
-    else if ($mod1 == 'Irc') $modname1 = _IRCAWARD;
-    else if ($mod1 == 'Links') $modname1 = _NAVLINKS;
-    else if ($mod1 == 'Wars') $modname1 = _LATESTWAR;
-    else if ($mod1 == 'News') $modname1 = _NAVNEWS;
-    else if ($mod1 == 'Sections') $modname1 = _NAVART;
-    else if ($mod1 == 'Server') $modname1 = _SERVERMONITOR;
-    else if ($mod1 == 'Forum') $modname1 = _NAVFORUM;
-    else if ($mod1 == 'Textbox') $modname1 = _BLOKSHOUT;
-    else if ($mod1 == 'Stats') $modname1 = _BLOKSTATS;
-    else if ($mod1 == 'Search') $modname1 = _BLOKSEARCH;
-    else $modname1 = $mod1;
+    $arrayModules = explode('|', $block['content']);
+    $module1 = $arrayModules[0];
+    $module2 = $arrayModules[1];
 
-    if ($mod2 == 'Gallery') $modname2 = _NAVGALLERY;
-    else if ($mod2 == 'Calendar') $modname2 = _NEXTWAR;
-    else if ($mod2 == 'Download') $modname2 = _NAVDOWNLOAD;
-    else if ($mod2 == 'Irc') $modname2 = _IRCAWARD;
-    else if ($mod2 == 'Links') $modname2 = _NAVLINKS;
-    else if ($mod2 == 'Wars') $modname2 = _LATESTWAR;
-    else if ($mod2 == 'News') $modname2 = _NAVNEWS;
-    else if ($mod2 == 'Sections') $modname2 = _NAVART;
-    else if ($mod2 == 'Server') $modname2 = _SERVERMONITOR;
-    else if ($mod2 == 'Forum') $modname2 = _NAVFORUM;
-    else if ($mod2 == 'Textbox') $modname2 = _BLOKSHOUT;
-    else if ($mod2 == 'Stats') $modname2 = _BLOKSTATS;
-    else if ($mod2 == 'Search') $modname2 = _BLOKSEARCH;
-    else $modname2 = $mod2;
-
-    $blok['content'] = '';
-
-    if ($mod2 != ""){
-        $blok['content'] .= '<div style="width: 48%; float: left" ><h3 style="text-align: center">' . $modname1 . '</h3>'."\n";
-        $bid = '';
+    if (defined(strtoupper($module1).'_MODNAME')) {
+        $modName1 = constant(strtoupper($module1).'_MODNAME');
     }
-	else{
-        $blok['content'] .= '<div style="width: 100%">'."\n";
-        $bid = $blok['bid'];
+    else {
+        $modName1 = $module1;
     }
 
-    $blok['content'] .= inc_bl1($mod1, $bid);
-
-    $blok['content'] .= '</div>'."\n";
-
-    if ($mod2 != ""){
-        $blok['content'] .= '<div style="float: left; width: 4%">&nbsp;</div><div style="float: left; width: 48%"><h3 style="text-align: center">' . $modname2 . '</h3>'."\n";
-        $b = 1;
-        $blok['content'] .= inc_bl2($mod2);
-        $blok['content'] .= '</div>'."\n";
+    if (defined(strtoupper($module2).'_MODNAME')) {
+        $modName2 = constant(strtoupper($module2).'_MODNAME');
+    }
+    else {
+        $modName2 = $module2;
     }
 
-    $blok['content'] .= '<div style="clear: both"></div>'."\n";
-    return $blok;
+    $block['content'] = '';
+
+    if (!empty($module1)){
+        $block['content'] .= '<div style="width: 48%; float: left" ><h3 style="text-align: center">' . $modName1 . '</h3>'."\n";
+        $block['content'] .= inc_bl1($module1);
+        $block['content'] .= '</div>'."\n";
+    }
+
+    if (!empty($module2)){
+        $block['content'] .= '<div style="float: left; width: 4%">&nbsp;</div><div style="float: left; width: 48%"><h3 style="text-align: center">' . $modName2 . '</h3>'."\n";
+        $block['content'] .= inc_bl2($module2);
+        $block['content'] .= '</div>'."\n";
+    }
+
+    $block['content'] .= '<div style="clear: both"></div>'."\n";
+    return $block;
 }
 
+function edit_block_center($block){
 
-function edit_block_center($bid){
-    global $nuked, $language;
+    $arrayModules = explode("|", $block['content']);
 
-    $sql = mysql_query('SELECT active, position, titre, module, content, type, nivo, page FROM ' . BLOCK_TABLE . ' WHERE bid = \'' . $bid . '\' ');
-    list($active, $position, $titre, $modul, $content, $type, $nivo, $pages) = mysql_fetch_array($sql);
-    $titre = printSecuTags($titre);
+    $module1 = $module2 = null;
 
-    if ($active == 3) $checked3 = 'selected="selected"';
-    else if ($active == 4) $checked4 = 'selected="selected"';
-    else $checked0 = 'selected="selected"';
+    if (array_key_exists(0, $arrayModules)) {
+        $module1 = $arrayModules[0];
+    }
 
-    $mod = explode("|", $content);
-    $mod1 = $mod[0];
-    $mod2 = $mod[1];
+    if (array_key_exists(1, $arrayModules)) {
+        $module2 = $arrayModules[1];
+    }
 
-    echo '<div class="content-box">',"\n" //<!-- Start Content Box -->
-			, '<div class="content-box-header"><h3>' , _BLOCKADMIN , '</h3>',"\n"
-			, '<a href="help/' , $language , '/block.html" rel="modal">',"\n"
-			, '<img style="border: 0;" src="help/help.gif" alt="" title="' , _HELP , '" /></a>',"\n"
-			, '</div>',"\n"
-			, '<div class="tab-content" id="tab2"><form method="post" action="index.php?file=Admin&amp;page=block&amp;op=modif_block">',"\n"
-			, '<table style="margin-left: auto;margin-right: auto;text-align: left;border: none;" cellspacing="0" cellpadding="2" >',"\n"
-			, '<tr><td><b>' , _TITLE , '</b></td><td><b>' , _BLOCK , '</b></td><td><b>' , _POSITION , '</b></td><td><b>' , _LEVEL , '</b></td></tr>',"\n"
-			, '<tr><td style="text-align:center;" ><input type="text" name="titre" size="40" value="' , $titre , '" /></td>',"\n"
-			, '<td align="center"><select name="active">',"\n"
-			, '<option value="3" ' , $checked3 , '>' , _CENTERBLOCK , '</option>',"\n"
-			, '<option value="4" ' , $checked4 , '>' , _FOOTERBLOCK , '</option>',"\n"
-			, '<option value="0" ' , $checked0 , '>' , _OFF , '</option></select></td>',"\n"
-			, '<td style="text-align:center;" ><input type="text" name="position" size="2" value="' , $position , '" /></td>',"\n"
-			, '<td align="center"><select name="nivo"><option>' , $nivo , '</option>',"\n"
-			, '<option>0</option>',"\n"
-			, '<option>1</option>',"\n"
-			, '<option>2</option>',"\n"
-			, '<option>3</option>',"\n"
-			, '<option>4</option>',"\n"
-			, '<option>5</option>',"\n"
-			, '<option>6</option>',"\n"
-			, '<option>7</option>',"\n"
-			, '<option>8</option>',"\n"
-			, '<option>9</option></select></td></tr><tr><td colspan="4"><b>' , _MODULE , ' 1 :</b> <select name="content[1]"><option value="">' , _NORANK , '</option>',"\n";
-
-    select_module($mod1);
-
-    echo '</select> <b>' , _MODULE , ' 2 :</b> <select name="content[2]"><option value="">' , _NORANK , '</option>',"\n";
-
-    select_module($mod2);
-
-    echo '</select></td></tr><tr><td colspan="4">&nbsp;</td></tr>',"\n"
-			, '<tr><td colspan="4" style="text-align:center;" ><b>' ,  _PAGESELECT , ' : </b></td></tr><tr><td colspan="4">&nbsp;</td></tr>',"\n"
-			, '<tr><td colspan="4" style="text-align:center;" ><select name="pages[]" size="8" multiple="multiple">',"\n";
-
-    select_mod2($pages);
-
-    echo '</select></td></tr><tr><td colspan="4" style="text-align:center;"><br />'
-			, '<input type="hidden" name="type" value="' , $type , '" />',"\n"
-			, '<input type="hidden" name="bid" value="' , $bid , '" />',"\n"
-			, '<input type="submit" value="' , _MODIFBLOCK , '" /></td></tr></table>',"\n"
-			, '<div style="text-align: center;"><br />[ <a href="index.php?file=Admin&amp;page=block"><b>' , _BACK , '</b></a> ]</div></form><br /></div></div>',"\n";
+?>
+                            <tr>
+                            <td>
+                                <strong><?php echo MODULE.' 1'; ?> :</strong>
+                                <select name="content[1]">
+<?php
+                                    select_module($module1);
+?>
+                                </select>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <strong><?php echo MODULE.' 2'; ?> :</strong>
+                                <select name="content[2]">
+<?php
+                                    select_module($module2);
+?>
+                                </select>
+                            </td>
+                        </tr>
+<?php
 }
 
 function modif_advanced_center($data){
-    if ($data['content'][1] != '' && $data['content'][2] != ''){
+    if (!empty($data['content'][1]) && !empty($data['content'][2])) {
         $sep = '|';
     }
     else{
         $sep = '';
     }
 
-    $content = $data['content'][1] . $sep . $data['content'][2];
+    $content = $data['content'][1].$sep.$data['content'][2];
     $data['content'] = $content;
     return $data;
 }
 
 function select_module($mod){
-    $handle = opendir('modules');
-    while (false !== ($f = readdir($handle))){
-        if ($f != '.' && $f != '..' && $f != 'CVS' && $f != 'index.html'  && !preg_match("/\./", $f)){
-            if ($mod == $f) $checked = 'selected="selected"';
-            else $checked = '';
+        $handle = opendir('modules');
+        while (false !== ($f = readdir($handle))){
+            if ($f != '.' && $f != '..' && $f != 'CVS' && $f != 'index.html'  && !preg_match("/\./", $f)){
+                if ($mod == $f) $checked = 'selected="selected"';
+                else $checked = '';
 
-            if (is_file('modules/' . $f . '/blok.php')) echo '<option value="' , $f , '" ' , $checked , '>' , $f , '</option>',"\n";
+                if (defined(strtoupper($f).'_MODNAME')) {
+                    $modName = constant(strtoupper($f).'_MODNAME');
+                }
+
+                if (is_file('modules/' . $f . '/blok.php')) echo '<option value="' , $f , '" ' , $checked , '>' , $modName , '</option>',"\n";
+            }
         }
+        closedir($handle);
     }
-    closedir($handle);
-}
 
-function inc_bl1($mod1, $bid){
+function inc_bl1($mod1){
     ob_start();
-    print eval("\$bid = \"$bid\";");
-    print eval(' include("modules/" . $mod1 . "/blok.php"); ');
+    $bid = null;
+    include_once 'modules/'.$mod1.'/blok.php';
     $blok_content = ob_get_contents();
     ob_end_clean();
     return $blok_content;
+
 }
 
 function inc_bl2($mod2){
     ob_start();
-    print eval(' include("modules/" . $mod2 . "/blok.php"); ');
+    $bid = null;
+    include_once 'modules/'.$mod2.'/blok.php';
     $blok_content = ob_get_contents();
     ob_end_clean();
     return $blok_content;
--- Includes/blocks/block_event.php
+++ Includes/blocks/block_event.php
@@ -15,7 +15,9 @@ translate('modules/Calendar/lang/' . $language . '.lang.php');
 function affich_block_event($blok){
     global $nuked, $bgcolor1, $bgcolor2, $bgcolor3, $file;
 
-    define ('ADAY', (61 * 60 * 24));
+    if (!defined('ADAY')) {
+        define ('ADAY', (61 * 60 * 24));
+    }
     $datearray = getdate();
 
     if (empty($_REQUEST['mo']) && empty($_REQUEST['ye'])){
@@ -42,7 +44,7 @@ function affich_block_event($blok){
         $year = $_REQUEST['ye'];
         $nextmonth = $_REQUEST['mo'] + 1;
         $prevmonth = $_REQUEST['mo']-1;
-		
+
         if ($nextmonth > 12){
             $nextmonth = 1;
             $nextyear = $year + 1;
@@ -113,7 +115,12 @@ function affich_block_event($blok){
             if ($nuked['birthday'] != 'off'){
                 $sql3 = mysql_query('SELECT user_id, age FROM ' . USER_DETAIL_TABLE);
                 while (list($tuid, $tage) = mysql_fetch_array($sql3)){
-                    list ($tjour, $tmois, $tan) = explode ('/', $tage);
+                    if(count(explode('/', $tage)) == 3){
+                        list ($tjour, $tmois, $tan) = explode ('/', $tage);
+                    }
+                    else{
+                        $tjour = $tmois = $tan = null;
+                    }
 
                     if ($nuked['birthday'] == 'team'){
                         $and = 'AND team > 0';
@@ -206,15 +213,15 @@ function affich_block_event($blok){
 
                             if ($amois == $month && $ajour == $event_date){
                                 $age = $year - $aan;
-								
+
                                 if ($month < $amois){
                                     $age = $age - 1;
                                 }
-								
+
                                 if ($event_date < $ajour && $month == $amois){
                                     $age = $age-1;
                                 }
-								
+
                                 $sql5 = mysql_query('SELECT pseudo FROM ' . USER_TABLE . ' WHERE id = \'' . $id_user . '\' ' . $and);
                                 list($pseudo) = mysql_fetch_array($sql5);
 
@@ -235,7 +242,7 @@ function affich_block_event($blok){
                 . '<a href="index.php?file=Calendar&amp;m=' . $month . '&amp;y=' . $year . '">'. $bd . $dayarray['mday'] . $bf . '</a></td>'."\n";
             }
             else{
-                $blok['content'] .= '<td align="center"><span style="text-align: center;">' . $bd . $dayarray[mday] . $bf . '</span></td>'."\n";
+                $blok['content'] .= '<td align="center"><span style="text-align: center;">' . $bd . $dayarray['mday'] . $bf . '</span></td>'."\n";
             }
 
             $start += ADAY;
@@ -247,52 +254,4 @@ function affich_block_event($blok){
     return $blok;
 }
 
-function edit_block_event($bid){
-    global $nuked, $language;
-
-    $sql = mysql_query('SELECT active, position, titre, module, content, type, nivo, page FROM ' . BLOCK_TABLE . ' WHERE bid = \'' . $bid . '\' ');
-    list($active, $position, $titre, $modul, $content, $type, $nivo, $pages) = mysql_fetch_array($sql);
-    $titre = printSecuTags($titre);
-
-    if ($active == 1) $checked1 = 'selected="selected"';
-    else if ($active == 2) $checked2 = 'selected="selected"';
-    else $checked0 = 'selected="selected"';
-
-    echo '<div class="content-box">'."\n" //<!-- Start Content Box -->
-	   . '<div class="content-box-header"><h3>'._BLOCKADMIN.'</h3>'."\n"
-	   . '<a href="help/'.$language.'/block.html" rel="modal">'."\n"
-	   . '<img style="border: 0;" src="help/help.gif" alt="" title="'._HELP.'" /></a>'."\n"
-	   . '</div>'."\n"
-	   . '<div class="tab-content" id="tab2"><form method="post" action="index.php?file=Admin&amp;page=block&amp;op=modif_block">'."\n"
-	   . '<table style="margin:0 auto;text-align: left;" cellspacing="0" cellpadding="2" border="0">'."\n"
-	   . '<tr><td><b>'._TITLE.'</b></td><td><b>'._BLOCK.'</b></td><td><b>'._POSITION.'</b></td><td><b>'._LEVEL.'</b></td></tr>'."\n"
-	   . '<tr><td style="text-align:center;" ><input type="text" name="titre" size="40" value="'.$titre.'" /></td>'."\n"
-	   . '<td style="text-align:center;"><select name="active">'."\n"
-	   . '<option value="1" '.$checked1.'>'._LEFT.'</option>'."\n"
-	   . '<option value="2" '.$checked2.'>'._RIGHT.'</option>'."\n"
-	   . '<option value="0" '.$checked0.'>'._OFF.'</option></select></td>'."\n"
-	   . '<td style="text-align:center;" ><input type="text" name="position" size="2" value="'.$position.'" /></td>'."\n"
-	   . '<td style="text-align:center;" ><select name="nivo"><option>'.$nivo.'</option>'."\n"
-	   . '<option>0</option>'."\n"
-	   . '<option>1</option>'."\n"
-	   . '<option>2</option>'."\n"
-	   . '<option>3</option>'."\n"
-	   . '<option>4</option>'."\n"
-	   . '<option>5</option>'."\n"
-	   . '<option>6</option>'."\n"
-	   . '<option>7</option>'."\n"
-	   . '<option>8</option>'."\n"
-	   . '<option>9</option></select></td></tr><tr><td colspan="4">&nbsp;</td></tr><tr><td colspan="4" style="text-align:center;" ><b>'._PAGESELECT.' :</b></td></tr><tr><td colspan="4">&nbsp;</td></tr>'."\n"
-	   . '<tr><td colspan="4" align="center"><select name="pages[]" size="8" multiple="multiple">'."\n";
-
-    select_mod2($pages);
-
-    echo '</select></td></tr><tr><td colspan="4" style="text-align:center;" ><br />'."\n"
-	   . '<input type="hidden" name="type" value="'.$type.'" />'."\n"
-	   . '<input type="hidden" name="bid" value="'.$bid.'" />'."\n"
-	   . '<input type="submit" name="send" value="'._MODIFBLOCK.'" />'."\n"
-	   . '</td></tr></table>'
-	   . '<div style="text-align: center;"><br />[ <a href="index.php?file=Admin&amp;page=block"><b>'._BACK.'</b></a> ]</div></form><br /></div></div>'."\n";
-
-}
 ?>
