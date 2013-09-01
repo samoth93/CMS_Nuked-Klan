513cce2dc54a039f9e09abf3334cc95d932d8dbf
Samoth <samoth93@gmail.com>
2013-09-02 01:35:56 +0200

    Add new administration base

---
+++ modules/Admin/config.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ *
+ * Tableaux des menus et sous-menu
+ *
+ */
+
+    // Sous-Menu des préférences
+    $arraySettings    = array(
+                            GENERAL_SETTINGS => array('index.php?file=Admin&amp;page=settings', 'icon-general-settings'),
+                            MYSQL            => array('index.php?file=Admin&amp;page=mysql', 'icon-mysql'),
+                            PHPINFO          => array('index.php?file=Admin&amp;page=phpinfo', 'icon-phpinfo'),
+                            ACTIONS          => array('index.php?file=Admin&amp;page=actions', 'icon-actions'),
+                            SQL_ERRORS       => array('index.php?file=Admin&amp;page=sql_errors', 'icon-warning')
+                        );
+
+    // Sous-Menu des gestions
+    $arrayManagements = array(
+                            USERS            => array('index.php?file=Admin&amp;page=users', 'icon-users'),
+                            GROUPS           => array('index.php?file=Admin&amp;page=groups', 'icon-groups'),
+                            MODULES          => array('index.php?file=Admin&amp;page=modules', 'icon-modules'),
+                            BLOCKS           => array('index.php?file=Admin&amp;page=blocks', 'icon-blocks'),
+                            SMILIES          => array('index.php?file=Admin&amp;page=smilies', 'icon-smilies'),
+                            GAMES            => array('index.php?file=Admin&amp;page=games', 'icon-games')
+                        );
+
+    // Initialisation du sous-menu des modules
+    $arrayModules     = array();
+
+    // Récupération de la liste des modules
+    $dbsModules = 'SELECT name
+                   FROM '.MODULES_TABLE.'
+                   WHERE status = "on"
+                   ORDER BY name ASC';
+    $dbeModules = mysql_query($dbsModules);
+
+    while (list($name) = mysql_fetch_array($dbeModules)) {
+        $translateName = $name;
+
+        // Si le nom du module a une traduction on traduit
+        if (defined(strtoupper($name).'_MODNAME')) {
+            $translateName = constant(strtoupper($name).'_MODNAME');
+        }
+
+        $arrayModules[$translateName] = array($name, 'icon'.strtoupper(substr($name, 0, 1)).substr($name, 1));
+    }
+
+    // On tri le sous-menu par rapport aux traductions
+    ksort($arrayModules);
+
+    // Sous-Menu divers
+    $arrayMisc        = array(
+                            OFFICIAL_FORUM   => array('http://www.nuked-klan.org/index.php?file=Forum', 'icon-forums'),
+                            LICENSE          => array('index.php?file=Admin&amp;page=license', 'icon-license'),
+                            ABOUT            => array('index.php?file=Admin&amp;page=about', 'icon-about')
+                        );
+
+    // Menu principal
+    $arrayMainNav     = array(
+                            PANEL            => array('index.php?file=Admin', 'icon-dashboard', null),
+                            SETTINGS         => array('#', 'icon-settings', $arraySettings),
+                            MANAGEMENTS      => array('#', 'icon-managements', $arrayManagements),
+                            MODULES          => array('#', 'icon-modules', $arrayModules),
+                            MISC             => array('#', 'icon-misc', $arrayMisc)
+                        );
+
+    // Si une administration de theme est fournie avec le theme on l'inclus dans le menu
+    if (file_exists('themes/'.$GLOBALS['nuked']['theme'].'/admin.php')) {
+        $arrayMainNav[THEME] = array('index.php?file=Admin&amp;page=template', 'icon-template', null);
+    }
+
+    // Définition du titre principal
+    $arrayPages =   array(
+                        SETTINGS    =>  array(
+                                            'settings' => array('title' => GENERAL_SETTINGS, 'link' => 'index.php?file=Admin&page=settings'),
+                                            'subNav'   => $arraySettings
+                                        ),
+                        MANAGEMENTS =>  array(
+                                            'smilies' => array('title' => SMILIES, 'link' => 'index.php?file=Admin&page=smilies'),
+                                            'subNav'   => $arrayManagements
+                                        )
+                    );
+
+    $breadcrumbs =  array(
+                        ADMIN_PANEL_TITLE => 'index.php?file=Admin'
+                    );
+
+    $mainTitle = ADMIN_PANEL_TITLE;
+
+    if ((isset($_REQUEST['file']) && $_REQUEST['file'] == 'Admin') && isset($_REQUEST['page'])) {
+        foreach ($arrayPages as $navTitle => $navContent) {
+            if (array_key_exists($_REQUEST['page'], $navContent)) {
+                $mainTitle = $navContent[$_REQUEST['page']]['title'];
+                $breadcrumbs[$navTitle]  = $navContent['subNav'];
+                $breadcrumbs[$mainTitle] = $navContent[$_REQUEST['page']]['link'];
+            }
+        }
+    }
+
+
+?>
--- modules/Admin/login.php
+++ modules/Admin/login.php
@@ -1,138 +1,131 @@
 <?php
-// -------------------------------------------------------------------------//
-// Nuked-KlaN - PHP Portal                                                  //
-// http://www.nuked-klan.org                                                //
-// -------------------------------------------------------------------------//
-// This program is free software. you can redistribute it and/or modify     //
-// it under the terms of the GNU General Public License as published by     //
-// the Free Software Foundation; either version 2 of the License.           //
-// -------------------------------------------------------------------------//
-defined('INDEX_CHECK') or die ('You can\'t run this file alone.');
-global $user, $nuked, $language;
+/**
+ * login.php (modules/Admin)
+ *
+ * Admin login
+ *
+ * @version 1.8
+ * @link http://www.nuked-klan.org Clan Management System for Gamers
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @copyright 2001-2013 Nuked-Klan (Registred Trademark)
+ */
 
-translate('modules/Admin/lang/' . $language . '.lang.php');
-translate('modules/User/lang/' . $language . '.lang.php');
-include_once(dirname(__FILE__) . '/../../Includes/hash.php');
+if (!defined("INDEX_CHECK")) exit('You can\'t run this file alone.');
 
-$url = 'index.php';
-$message = '';
+// Inclusion de la langue
+nkTranslate('modules/Admin/lang/'.$GLOBALS['language'].'.lang.php');
 
-if(isset($user) && empty($user)){
-    redirect('index.php?file=404', 0);
+// Inclusion du layout de l'administration
+require_once 'modules/Admin/views/layout.php';
+
+// Redirection des visiteurs sur un 404
+if(nkHasVisitor()){
+    header('location: index.php?file=404');
     exit();
 }
 
-$hasModAccess = nkAccessModule('Admin');
+// On vérifie que l'utilisateur à le droit de se connecter à l'administration
+if (nkAccessModule('Admin') === true) {
+    // Dans le cas d'une requete ajax
+    if(isset($_REQUEST['nuked_nude']) && $_REQUEST['nuked_nude'] == 'admin' && !empty($GLOBALS['user']['nickName'])) {
+        // On vérifie que tous les champs on bien été rempli il qu'ils sont conformes aux attentes
+        $arrayRequest = array('mail', 'password');
+        $error = false;
 
-function open_admin(){
-    global $nuked;
-    echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-    <html xmlns="http://www.w3.org/1999/xhtml">
-        <head>
-            <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-            <title>' . $nuked['name'] . ' - ' . _ADMINISTRATION . '</title>
-            <link rel="stylesheet" href="modules/Admin/css/reset.css" type="text/css" media="screen" />
-            <link rel="stylesheet" href="modules/Admin/css/style.css" type="text/css" media="screen" />
-            <link rel="stylesheet" href="modules/Admin/css/invalid.css" type="text/css" media="screen" />
-        </head>
-        <body id="login">
-            <div id="login-wrapper" class="png_bg">
-                <div id="login-top">
-                    <h1>' . $nuked['name'] . ' - ' . _ADMINSESSION . '</h1>
-                    <img id="logo" src="modules/Admin/images/logo.png" alt="NK Logo" />
-                </div>
-                <div id="login-content">';
-}
+        foreach ($arrayRequest as $request) {
+            $error = true;
 
-function close_admin(){
-    echo '</div>
-            </div>
-        </body>
-    </html>';
-}
-
-if ($hasModAccess === true)
-{
-    if ($user && isset($_POST['admin_password']) && $_POST['admin_password'] != '')
-    {
-        // $GLOBALS['cookieAdmin'] = $nuked['cookiename'] . '_admin_session';
-
-        $sql = mysql_query("SELECT pseudo, pass FROM " . USER_TABLE . " WHERE id = '" . $GLOBALS['user']['id'] . "'");
-        list($pseudo, $hash) = mysql_fetch_array($sql);
-        $check = mysql_num_rows($sql);
-
-        if ($check == 1 && Check_Hash($_POST['admin_password'], $hash))
-        {
-            list($pseudo) = mysql_fetch_array($sql);
-
-            if ($_POST['formulaire'] == 0 && $_SERVER['HTTP_REFERER'] != '')
-            {
-                list($url_ref, $redirect) = explode('?', $_SERVER['HTTP_REFERER']);
-                if ($redirect == 'file=Admin&page=login') $redirect = 'file=Admin';
-                $url = 'index.php?' . $redirect;
+            if (isset($_REQUEST[$request]) && !empty($_REQUEST[$request])) {
+                ${$request} = $_REQUEST[$request];
+                $error = false;
             }
-            else $url = 'index.php?file=Admin';
+        }
+
+        if ($error === true) {
+            echo EMPTY_FIELDS;
+            exit();
+        }
+
+        $nickName  = mysql_real_escape_string($GLOBALS['user']['nickName']);
+
+        $dbsLogin  = 'SELECT mail, pass
+                     FROM '.USER_TABLE.'
+                     WHERE pseudo = "'.$nickName.'"
+                     LIMIT 1';
+        $dbeLogin  = mysql_query($dbsLogin) or die(mysql_error());
+
+        $dataLogin = mysql_fetch_assoc($dbeLogin);
+
+        $errorLogin = null;
+        $arrayErrorMsg = array( 1 => BAD_MAIL, 2 => BAD_PASSWORD);
+
+        if ($dataLogin['mail'] != $mail) {
+            $errorLogin = 1;
+        }
+
+        if (!Check_Hash($password, $dataLogin['pass'])) {
+            if (is_null($errorLogin)) {
+                $errorLogin = 2;
+            }
+        }
+
+        if (is_null($errorLogin)) {
+            $texteaction = mysql_real_escape_string(ACTION_CONNECT);
+            mysql_query('INSERT INTO '.$GLOBALS['nuked']['prefix'].'_action VALUES ("", '.time().'", "'.$GLOBALS['user']['id'].'", "'.$texteaction.'")');
 
             $_SESSION['admin'] = true;
-            // Action
-            $texteaction = _ACTIONCONNECT;
-            $acdate = time();
-            $action = mysql_query("INSERT INTO ". $nuked['prefix'] ."_action  (`date` , `pseudo` , `action`)  VALUES ('".$acdate."', '".$GLOBALS['user']['id']."', '".$texteaction."')");
-            //Fin action
 
-            $message = (isset($_COOKIE[$GLOBALS['cookieSession']])) ? _ADMINPROGRESS : _ERRORCOOKIE;
+            echo LOGIN_SUCCESS;
+
+            redirect('index.php?file=Admin', 3);
         }
-        else
-        {
-            $message = _BADLOGADMIN;
-            $url = 'index.php?file=Admin';
+        else {
+            echo $arrayErrorMsg[$errorLogin];
         }
 
-            open_admin();
-            echo '<div style="text-align: center">' . $message . '</div>';
-            close_admin();
-
-            redirect($url, 2);
     }
-    else
-    {
-        if (!$user) redirect('index.php?file=User&op=login_screen', 0);
-        else if (array_key_exists('nuked_nude', $_REQUEST) && $_REQUEST['nuked_nude'] == 'admin') redirect('index.php?file=Admin', 0);
-        else
-        {
-            $check = $_POST ? 1 : 0;
-
-            open_admin();
-
-            echo '<form action="index.php?file=Admin&amp;nuked_nude=login" method="post">
-                    <p>
-                        <label>' . _NICK . '</label>
-                        <input class="text-input" type="text" name="admin_pseudo" value="' . $GLOBALS['user']['nickName'] . '" maxlenght="180" />
-                    </p>
-                    <div class="clear"></div>
-                    <p>
-                        <label>' . _PASSWORD . '</label>
-                        <input class="text-input" type="password" name="admin_password" maxlength="40" />
-                    </p>
-
-                    <div class="clear"></div>
-                    <p>
-                        <input class="button" type="submit" value="' . _TOLOG . '" />
-						<input class="button" type="button" value="' . _TOBACK . '" style="margin-right: 10px" onclick="javascript:history.back()" />
-                        <input type="hidden" name="formulaire" value="' . $check  . '" />
-                    </p>
-                </form>';
-
-            close_admin();
-        }
+    else{
+        // On affiche le header du layout
+        adminHeader();
+?>
+    <div class="loginWrapper">
+        <form id="login" class="validateForm" action="index.php?file=Admin&amp;nuked_nude=login" method="post">
+            <div class="loginPic">
+                <img alt="" src="<?php echo $GLOBALS['user']['avatar']; ?>">
+                <span><?php echo $GLOBALS['user']['nickName']; ?></span>
+            </div>
+            <input type="text" id="loginMail" class="loginEmail validate[required,custom[email]]" placeholder="<?php echo MAIL; ?>" name="mail" />
+            <input type="password" id="loginPassword" class="loginPassword validate[required]" placeholder="<?php echo PASSWORD; ?>" name="password" />
+            <div class="logControl">
+                <div class="memory">
+                    <input type="checkbox" checked="checked" class="check" id="remember_me" />
+                    <label for="remember_me"><?php echo REMEMBER_ME; ?></label>
+                </div>
+                <input type="submit" id="loginSubmit" class="buttonM bBlue" value="<?php echo LOGIN; ?>" name="submit" />
+            </div>
+        </form>
+    </div>
+    <div id="loginMessage" class="ajaxMessage ajaxLoading" title="<?php echo LOGIN; ?>">
+        <p><?php echo CHECK_IN_PROGRESS; ?></p>
+    </div>
+<?php
+        // On affiche le footer du layout
+        adminFooter();
     }
 }
-else
-{
-    open_admin();
-    echo '<div style="text-align: center">' . _ZONEADMIN . '<br /><a href="javascript:history.back()"><b>' . _BACK . '</b></a></div>';
-    close_admin();
+else {
+    // On affiche le header du layout
+    adminHeader();
+?>
+    <div style="text-align: center;">
+        <p><?php echo ZONEADMIN; ?></p>
+        <a class="button" href="javascript:history.back()">
+            <b><?php echo BACK; ?></b>
+        </a>
+    </div>
+<?php
+    // On affiche le footer du layout
+    adminFooter();
 }
 
 ?>
