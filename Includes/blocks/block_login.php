99aebd0cc3009298ef46a510588a12422b6a2649
Samoth <samoth93@gmail.com>
2013-09-02 01:24:58 +0200

    Add groupsManagement in blocks Html, Language, Login + Fix php errors

--- Includes/blocks/block_login.php
+++ Includes/blocks/block_login.php
@@ -1,82 +1,79 @@
 <?php
-// -------------------------------------------------------------------------//
-// Nuked-KlaN - PHP Portal                                                  //
-// http://www.nuked-klan.org                                                //
-// -------------------------------------------------------------------------//
-// This program is free software. you can redistribute it and/or modify     //
-// it under the terms of the GNU General Public License as published by     //
-// the Free Software Foundation; either version 2 of the License.           //
-// -------------------------------------------------------------------------//
-if (!defined("INDEX_CHECK")){
-	exit('You can\'t run this file alone.');
-}
+/**
+ * block_login.php
+ *
+ * Display and Admin of login block
+ *
+ * @version 1.8
+ * @link http://www.nuked-klan.org Clan Management System for Gamers
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @copyright 2001-2013 Nuked-Klan (Registred Trademark)
+ */
 
-function affich_block_login($blok){
+function affich_block_login($block){
     global $user, $nuked, $bgcolor3, $bgcolor1 ;
 
-
-    if(!empty($blok['content'])){
-        list($login, $messpv, $members, $online, $avatar) = explode('|', $blok['content']);
+    if(!empty($block['content'])){
+        list($login, $messpv, $members, $whoisonline, $showavatar) = explode('|', $block['content']);
     }
     else{
-        $login = $messpv = $members = $online = $avatar = 'on';
+        $login = $messpv = $members = $whoisonline = $showavatar = 'on';
     }
-    $blok['content'] = '';
 
 	$c = 0;
 
 	if($login != 'off'){
-		if (!$user){
-			$blok['content'] = '<form action="index.php?file=User&amp;nuked_nude=index&amp;op=login" method="post">'."\n"
+		if (nkHasVisitor()){
+			$block['content'] = '<form action="index.php?file=User&amp;nuked_nude=index&amp;op=login" method="post">'."\n"
 			. '<table style="margin-left: auto;margin-right: auto;text-align: left;">'."\n"
-			. '<tr><td>' . _NICK . ' :</td><td><input type="text" name="pseudo" size="10" maxlength="250" /></td></tr>'."\n"
-			. '<tr><td>' . _PASSWORD . ' :</td><td><input type="password" name="pass" size="10" maxlength="15" /></td></tr>'."\n"
-			. '<tr><td colspan="2"><input type="checkbox" class="checkbox" name="remember_me" value="ok" checked="checked" />&nbsp;' . _SAVE . '</td></tr>'."\n"
-			. '<tr><td colspan="2" align="center"><input type="submit" value="' . _BLOGIN . '" /></td></tr>'."\n"
-			. '<tr><td colspan="2"><a href="index.php?file=User&amp;op=reg_screen">' . _REGISTER . '</a><br />'."\n"
-			. '<a href="index.php?file=User&amp;op=oubli_pass">' . _FORGETPASS . '</a> ?</td></tr></table></form>'."\n";
+			. '<tr><td>' . NICKNAME . ' :</td><td><input type="text" name="pseudo" size="10" maxlength="250" /></td></tr>'."\n"
+			. '<tr><td>' . PASSWORD . ' :</td><td><input type="password" name="pass" size="10" maxlength="15" /></td></tr>'."\n"
+			. '<tr><td colspan="2"><input type="checkbox" class="checkbox" name="remember_me" value="ok" checked="checked" />&nbsp;' . REMEMBER_ME . '</td></tr>'."\n"
+			. '<tr><td colspan="2" align="center"><input type="submit" value="' . LOGIN . '" /></td></tr>'."\n"
+			. '<tr><td colspan="2"><a href="index.php?file=User&amp;op=reg_screen">' . REGISTER . '</a><br />'."\n"
+			. '<a href="index.php?file=User&amp;op=oubli_pass">' . FORGOTTEN_PASSWORD . '</a> ?</td></tr></table></form>'."\n";
 		}
 		else{
-			$blok['content'] = '<div style="text-align: center;">' . _WELCOME . ', <b>' . $GLOBALS['user']['nickName'] . '</b><br /><br />'."\n";
-			if ($avatar != 'off'){
+			$block['content'] = '<div style="text-align: center;">' . WELCOME . ', <b>' . $GLOBALS['user']['nickName'] . '</b><br /><br />'."\n";
+			if ($showavatar != 'off'){
 				$sql_avatar=mysql_query('SELECT avatar FROM ' . USER_TABLE . ' WHERE id = \'' . $GLOBALS['user']['id'] . '\' ');
 				list($avatar_url) = mysql_fetch_array($sql_avatar);
-				if($avatar_url) $blok['content'] .= '<img src="' . $avatar_url . '" style="border:1px ' . $bgcolor3 . ' dashed; width:100px; background:' . $bgcolor1 . '; padding:2px;" alt="' . $GLOBALS['user']['nickName'] . ' avatar" /><br /><br />';
+				if($avatar_url) $block['content'] .= '<img src="' . $avatar_url . '" style="border:1px ' . $bgcolor3 . ' dashed; width:100px; background:' . $bgcolor1 . '; padding:2px;" alt="' . $GLOBALS['user']['nickName'] . ' avatar" /><br /><br />';
 			}
-			$blok['content'] .= '<a href="index.php?file=User">' . _ACCOUNT . '</a> / <a href="index.php?file=User&amp;nuked_nude=index&amp;op=logout">' . _LOGOUT . '</a></div>'."\n";
+			$block['content'] .= '<a href="index.php?file=User">' . ACCOUNT . '</a> / <a href="index.php?file=User&amp;nuked_nude=index&amp;op=logout">' . LOGOUT . '</a></div>'."\n";
 		}
 		$c++;
 	}
 
-    if($messpv != 'off' && (array_key_exists(0, $user) && $GLOBALS['user']['id'] != '')){
-		if ($c > 0) $blok['content'] .= '<hr style="height: 1px;" />'."\n";
+    if($messpv != 'off' && !nkHasVisitor()){
+		if ($c > 0) $block['content'] .= '<hr style="height: 1px;" />'."\n";
 
 		$sql2 = mysql_query('SELECT mid FROM ' . USERBOX_TABLE . ' WHERE user_for = \'' . $GLOBALS['user']['id'] . '\' AND status = 1');
 		$nb_mess_lu = mysql_num_rows($sql2);
 
-		$blok['content'] .= '&nbsp;<img width="14" height="12" src="images/message.gif" alt="" />&nbsp;<span style="text-decoration: underline"><b>' . _MESSPV . '</b></span><br />'."\n";
+		$block['content'] .= '&nbsp;<img width="14" height="12" src="images/message.gif" alt="" />&nbsp;<span style="text-decoration: underline"><b>' . PRIVATE_MESSAGE . '</b></span><br />'."\n";
 
 		if ($GLOBALS['user']['nbMess'] > 0){
-			$blok['content'] .= '&nbsp;<b><big>·</big></b>&nbsp;' . _NOTREAD . ' : <a href="index.php?file=Userbox"><b>' . $GLOBALS['user']['nbMess'] . '</b></a>'."\n";
+			$block['content'] .= '&nbsp;<b><big>·</big></b>&nbsp;' . UNREADS . ' : <a href="index.php?file=Userbox"><b>' . $GLOBALS['user']['nbMess'] . '</b></a>'."\n";
 		}
 		else{
-			$blok['content'] .= '&nbsp;<b><big>·</big></b>&nbsp;' . _NOTREAD . ' : <b>' . $GLOBALS['user']['nbMess'] . '</b>'."\n";
+			$block['content'] .= '&nbsp;<b><big>·</big></b>&nbsp;' . UNREADS . ' : <b>' . $GLOBALS['user']['nbMess'] . '</b>'."\n";
 		}
 
 		if ($nb_mess_lu > 0){
-			$blok['content'] .= '<br />&nbsp;<b><big>·</big></b>&nbsp;' . _READ . ' : <a href="index.php?file=Userbox"><b>' . $nb_mess_lu . '</b></a>'."\n";
+			$block['content'] .= '<br />&nbsp;<b><big>·</big></b>&nbsp;' . READS . ' : <a href="index.php?file=Userbox"><b>' . $nb_mess_lu . '</b></a>'."\n";
 		}
 		else{
-			$blok['content'] .= '<br />&nbsp;<b><big>·</big></b>&nbsp;' . _READ . ' : <b>' . $nb_mess_lu . '</b>'."\n";
+			$block['content'] .= '<br />&nbsp;<b><big>·</big></b>&nbsp;' . READS . ' : <b>' . $nb_mess_lu . '</b>'."\n";
 		}
 
 		$c++;
     }
 
 	if ($members != 'off'){
-		if ($c > 0) $blok['content'] .= '<hr style="height: 1px;" />'."\n";
+		if ($c > 0) $block['content'] .= '<hr style="height: 1px;" />'."\n";
 
-    	$blok['content'] .= '&nbsp;<img width="16" height="13" src="images/memberslist.gif" alt="" />&nbsp;<span style="text-decoration: underline"><b>' . _MEMBERS . '</b></span><br />'."\n";
+    	$block['content'] .= '&nbsp;<img width="16" height="13" src="images/memberslist.gif" alt="" />&nbsp;<span style="text-decoration: underline"><b>' . MEMBERS . '</b></span><br />'."\n";
 
     	$sql_users = mysql_query('SELECT id FROM ' . USER_TABLE . ' WHERE niveau < 3');
     	$nb_users = mysql_num_rows($sql_users);
@@ -87,17 +84,17 @@ function affich_block_login($blok){
     	$sql_lastmember = mysql_query('SELECT pseudo FROM ' . USER_TABLE . ' ORDER BY date DESC LIMIT 0, 1');
     	list($lastmember) = mysql_fetch_array($sql_lastmember);
 
-    	$blok['content'] .= '&nbsp;<b><big>·</big></b>&nbsp;' . _ADMINS . ' : <b>' . $nb_admin . '</b><br />&nbsp;<b><big>·</big></b>&nbsp;' . _MEMBERS . ' :'
-    	. '&nbsp;<b>' . $nb_users . '</b> [<a href="index.php?file=Members">' . _LIST . '</a>]<br />'."\n"
-		. '&nbsp;<b><big>·</big></b>&nbsp;' . _LASTMEMBER . ' : <a href="index.php?file=Members&amp;op=detail&amp;autor=' . urlencode($lastmember) . '"><b>' . $lastmember . '</b></a>'."\n";
+    	$block['content'] .= '&nbsp;<b><big>·</big></b>&nbsp;' . ADMINISTRATOR . ' : <b>' . $nb_admin . '</b><br />&nbsp;<b><big>·</big></b>&nbsp;' . MEMBERS . ' :'
+        . '&nbsp;<b>' . $nb_users . '</b> [<a href="index.php?file=Members">' . NK_LIST . '</a>]<br />'."\n"
+        . '&nbsp;<b><big>·</big></b>&nbsp;' . LAST_MEMBER . ' : <a href="index.php?file=Members&amp;op=detail&amp;autor=' . urlencode($lastmember) . '"><b>' . $lastmember . '</b></a>'."\n";
 
 		 $c++;
 	}
 
-	if ($online != 'off'){
-		if ($c > 0) $blok['content'] .= '<hr style="height: 1px;" />'."\n";
+	if ($whoisonline != 'off'){
+		if ($c > 0) $block['content'] .= '<hr style="height: 1px;" />'."\n";
 
-    	$blok['content'] .= '&nbsp;<img width="16" height="13" src="images/online.gif" alt="" />&nbsp;<span style="text-decoration: underline"><b>' . _WHOISONLINE . '</b></span><br />'."\n";
+    	$block['content'] .= '&nbsp;<img width="16" height="13" src="images/online.gif" alt="" />&nbsp;<span style="text-decoration: underline"><b>' . WHO_IS_ONLINE . '</b></span><br />'."\n";
 
     	$nb = nbvisiteur();
 
@@ -108,7 +105,7 @@ function affich_block_login($blok){
 				   $user_online .= '&nbsp;<b><big>·</big></b>&nbsp;<b>' . $nom . '</b><br />';
 			}
 
-			$user_list = '&nbsp;[<a href="#" onmouseover="AffBulle(\'&nbsp;&nbsp;' . _WHOISONLINE . '\', \'' . htmlentities(mysql_real_escape_string($user_online), ENT_NOQUOTES, 'ISO-8859-1') . '\', 150)" onmouseout="HideBulle()">' . _LIST . '</a>]';
+			$user_list = '&nbsp;[<a href="#" onmouseover="AffBulle(\'&nbsp;&nbsp;' . WHO_IS_ONLINE . '\', \'' . htmlentities(mysql_real_escape_string($user_online), ENT_NOQUOTES, 'ISO-8859-1') . '\', 150)" onmouseout="HideBulle()">' . NK_LIST . '</a>]';
 			}
     	else{
 			$user_list = '';
@@ -121,109 +118,93 @@ function affich_block_login($blok){
 				   $admin_online .= '&nbsp;<b><big>·</big></b>&nbsp;<b>' . $name . '</b><br />';
 			}
 
-			$admin_list = '&nbsp;[<a href="#" onmouseover="AffBulle(\'&nbsp;&nbsp;' . _WHOISONLINE . '\', \'' . htmlentities(mysql_real_escape_string($admin_online), ENT_NOQUOTES, 'ISO-8859-1') . '\', 150)" onmouseout="HideBulle()">' . _LIST . '</a>]';
+			$admin_list = '&nbsp;[<a href="#" onmouseover="AffBulle(\'&nbsp;&nbsp;' . WHO_IS_ONLINE . '\', \'' . htmlentities(mysql_real_escape_string($admin_online), ENT_NOQUOTES, 'ISO-8859-1') . '\', 150)" onmouseout="HideBulle()">' . NK_LIST . '</a>]';
 		}
 		else{
 			$admin_list = '';
 		}
 
-		$blok['content'] .= '&nbsp;<b><big>·</big></b>&nbsp;' . _VISITOR;
-		if ($nb[0] > 1) $blok['content'] .= 's';
-		$blok['content'] .= ' : <b>' . $nb[0] . '</b><br />&nbsp;<b><big>·</big></b>&nbsp;' . _MEMBER;
-		if ($nb[1] > 1) $blok['content'] .= 's';
-		$blok['content'] .= ' : <b>' . $nb[1] . '</b>' . $user_list . '<br />&nbsp;<b><big>·</big></b>&nbsp;' . _ADMIN;
-		if ($nb[2] > 1) $blok['content'] .= 's';
-		$blok['content'] .= ' : <b>' . $nb[2] . '</b>' . $admin_list . '<br />'."\n";
+		$block['content'] .= '&nbsp;<b><big>·</big></b>&nbsp;' . VISITOR;
+		if ($nb[0] > 1) $block['content'] .= 's';
+		$block['content'] .= ' : <b>' . $nb[0] . '</b><br />&nbsp;<b><big>·</big></b>&nbsp;' . MEMBER;
+		if ($nb[1] > 1) $block['content'] .= 's';
+		$block['content'] .= ' : <b>' . $nb[1] . '</b>' . $user_list . '<br />&nbsp;<b><big>·</big></b>&nbsp;' . ADMINISTRATOR;
+		if ($nb[2] > 1) $block['content'] .= 's';
+		$block['content'] .= ' : <b>' . $nb[2] . '</b>' . $admin_list . '<br />'."\n";
 
 		$c++;
    }
 
-   return $blok;
+   return $block;
 }
 
-function edit_block_login($bid){
-    global $nuked, $language;
+function edit_block_login($block){
 
-    $sql = mysql_query('SELECT active, position, titre, module, content, type, nivo, page FROM ' . BLOCK_TABLE . ' WHERE bid = \'' . $bid . '\' ');
-    list($active, $position, $titre, $modul, $content, $type, $nivo, $pages) = mysql_fetch_array($sql);
-    $titre = printSecuTags($titre);
+    $arrayOptions = array(
+                        'login'       => 'on',
+                        'messpv'      => 'on',
+                        'members'     => 'on',
+                        'whoisonline' => 'on',
+                        'showavatar'  => 'on'
+                    );
 
-    if(!empty($content)){
-        list($login, $messpv, $members, $online, $avatar) = explode('|', $content);
+    $arrayConstants = array(
+                        'login' => 'LOGIN',
+                        'messpv' => 'PRIVATE_MESSAGE',
+                        'members' => 'MEMBERS',
+                        'whoisonline' => 'WHO_IS_ONLINE',
+                        'showavatar' => 'DISPLAY_AVATAR'
+                      );
+
+    if(!empty($block['content'])){
+        list($login, $messpv, $members, $whoisonline, $showavatar) = explode('|', $block['content']);
+
+        foreach ($arrayOptions as $option => $value) {
+            $arrayOptions[$option] = ${$option};
+        }
     }
-    else{
-        $login = $messpv = $members = $online = $avatar = 'on';
-    }
 
-    $checked0 = $checked1 = $checked2 = '';
+    foreach ($arrayOptions as $option => $value):
+        if ($value == 'on') {
+            $checked = ' checked="checked" ';
+        }
+        else{
+            $checked = '';
+        }
+?>
+        <tr>
+            <td>
+                <strong style="display:inline-block;width:120px;"><?php echo constant($arrayConstants[$option]); ?></strong>
+                <div class="checkboxSliderWrapper">
+                     <div class="onoffswitch">
+                        <input type="checkbox" name="<?php echo $option; ?>" class="onoffswitch-checkbox" id="block-<?php echo $option; ?>" <?php echo $checked; ?>>
+                        <label class="onoffswitch-label" for="block-<?php echo $option; ?>">
+                            <div class="onoffswitch-inner"></div>
+                            <div class="onoffswitch-switch"></div>
+                        </label>
+                    </div>
+                </div>
+            </td>
+        </tr>
+<?php
 
-    if ($active == 1) $checked1 = 'selected="selected"';
-    else if ($active == 2) $checked2 = 'selected="selected"';
-    else $checked0 = 'selected="selected"';
-
-    if ($login == 'off') $checked3 = 'selected="selected"'; else $checked3 = '';
-    if ($messpv == 'off') $checked4 = 'selected="selected"'; else $checked4 = '';
-    if ($members == 'off') $checked5 = 'selected="selected"'; else $checked5 = '';
-    if ($online == 'off') $checked6 = 'selected="selected"'; else $checked6 = '';
-	if ($avatar == 'off') $checked7 = 'selected="selected"'; else $checked7 = '';
-
-    echo '<div class="content-box">',"\n" //<!-- Start Content Box -->
-			, '<div class="content-box-header"><h3>' , _BLOCKADMIN , '</h3>',"\n"
-			, '<a href="help/' , $language , '/block.html" rel="modal">',"\n"
-			, '<img style="border: 0;" src="help/help.gif" alt="" title="' , _HELP , '" /></a>',"\n"
-			, '</div>',"\n"
-			, '<div class="tab-content" id="tab2"><form method="post" action="index.php?file=Admin&amp;page=block&amp;op=modif_block">',"\n"
-			, '<table style="margin-left: auto;margin-right: auto;text-align: left;" cellspacing="0" cellpadding="2" border="0">',"\n"
-			, '<tr><td><b>' , _TITLE , '</b></td><td><b>' , _BLOCK , '</b></td><td><b>' , _POSITION , '</b></td><td><b>' , _LEVEL , '</b></td></tr>',"\n"
-			, '<tr><td align="center"><input type="text" name="titre" size="40" value="' , $titre , '" /></td>',"\n"
-			, '<td align="center"><select name="active">',"\n"
-			, '<option value="1" ' , $checked1 , '>' , _LEFT , '</option>',"\n"
-			, '<option value="2" ' , $checked2 , '>' , _RIGHT , '</option>',"\n"
-			, '<option value="0" ' , $checked0 , '>' , _OFF , '</option></select></td>',"\n"
-			, '<td align="center"><input type="text" name="position" size="2" value="' , $position , '" /></td>',"\n"
-			, '<td align="center"><select name="nivo"><option>' , $nivo , '</option>',"\n"
-			, '<option>0</option>',"\n"
-			, '<option>1</option>',"\n"
-			, '<option>2</option>',"\n"
-			, '<option>3</option>',"\n"
-			, '<option>4</option>',"\n"
-			, '<option>5</option>',"\n"
-			, '<option>6</option>',"\n"
-			, '<option>7</option>',"\n"
-			, '<option>8</option>',"\n"
-			, '<option>9</option></select></td></tr>',"\n"
-			, '<tr><td colspan="4">' , _LOGIN , ' : <select name="login">',"\n"
-			, '<option value="on">' , _YES , '</option>',"\n"
-			, '<option value="off" ' , $checked3 , '>' , _NO , '</option></select>',"\n"
-			, '&nbsp;' , _MESSPV , '  : <select name="messpv">',"\n"
-			, '<option value="on">' , _YES , '</option>',"\n"
-			, '<option value="off" ' , $checked4 , '>' , _NO , '</option></select>',"\n"
-			, '&nbsp;' , _MEMBERS , ' : <select name="members">',"\n"
-			, '<option value="on">' , _YES , '</option>',"\n"
-			, '<option value="off" ' , $checked5 , '>' , _NO , '</option></select>',"\n"
-			, '</td></tr><tr><td colspan="4">&nbsp;' , _WHOISONLINE , ' : <select name="online">',"\n"
-			, '<option value="on">' , _YES , '</option>',"\n"
-			, '<option value="off" ' , $checked6 , '>' , _NO , '</option></select>',"\n"
-			, '&nbsp;' , _SHOWAVATAR , ' : <select name="avatar">',"\n"
-			, '<option value="on">' , _YES , '</option>',"\n"
-			, '<option value="off" ' , $checked7 , '>' , _NO , '</option></select>',"\n"
-			, '</td></tr><tr><td colspan="4">&nbsp;</td></tr>',"\n"
-			, '<tr><td colspan="4" align="center"><b>' , _PAGESELECT , ' :</b></td></tr><tr><td colspan="4">&nbsp;</td></tr>',"\n"
-			, '<tr><td colspan="4" align="center"><select name="pages[]" size="8" multiple="multiple">',"\n";
-
-    select_mod2($pages);
-
-    echo '</select></td></tr><tr><td colspan="4" align="center"><br />',"\n"
-		, '<input type="hidden" name="type" value="' , $type , '" />',"\n"
-		, '<input type="hidden" name="bid" value="' , $bid , '" />',"\n"
-		, '<input type="submit" name="send" value="' , _MODIFBLOCK , '" />',"\n"
-		, '</td></tr></table>'
-		, '<div style="text-align: center;"><br />[ <a href="index.php?file=Admin&amp;page=block"><b>' , _BACK , '</b></a> ]</div></form><br /></div></div>',"\n";
-
+    endforeach;
 }
 
 function modif_advanced_login($data){
-	$data['content'] = $data['login'] . '|' . $data['messpv'] . '|' . $data['members'] . '|' . $data['online']. '|' . $data['avatar'];
+
+    $arrayOptions = array('login','messpv','members','whoisonline','showavatar');
+
+    foreach ($arrayOptions as $option) {
+        if (array_key_exists($option, $data)) {
+            ${$option} = mysql_real_escape_string($data[$option]);
+        }
+        else{
+            ${$option} = 'off';
+        }
+    }
+
+	$data['content'] = $login.'|'.$messpv.'|'.$members.'|'.$whoisonline.'|'.$showavatar;
 	return $data;
 }
 ?>
