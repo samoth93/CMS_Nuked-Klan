8d4059e6b5389d7e967f375e2c5d8787a092e95d
Samoth <samoth93@gmail.com>
2013-09-02 01:21:40 +0200

    Add groupManagement in template Impact_Nk What? who said useless?

--- themes/Impact_Nk/blocks/test.php
+++ themes/Impact_Nk/blocks/test.php
@@ -11,20 +11,20 @@ if($nb[0]>1){$s0="s";}
 if($nb[1]>1){$s1="s";}
 if($nb[2]>1){$s2="s";}
 
-if($user){
+if(!nkHasVisitor()){
     $sql2 = mysql_query("SELECT mid FROM " . USERBOX_TABLE . " WHERE user_for = '" . $GLOBALS['user']['id'] . "' AND status = 0");
     $nb_mess_lu = mysql_num_rows($sql2);
     list($mid) = mysql_fetch_array($sql2);
 
     if($nb_mess_lu>0){
-        $mess="<a href=\"index.php?file=Userbox&amp;op=show_message&amp;mid=".$mid."\">"._YOUVE." ".$nb_mess_lu." </a> "._INNEW."</a>.";
+        $mess="<a href=\"index.php?file=Userbox&amp;op=show_message&amp;mid=".$mid."\">".YOU_HAVE." ".$nb_mess_lu." </a> "._INNEW."</a>.";
     }
     else{
-        $mess="<a href=\"index.php?file=Userbox\">"._YOUVE." 0 "._INNEW."</a>";
+        $mess="<a href=\"index.php?file=Userbox\">". YOU_HAVE." 0 "._INNEW."</a>";
     }
 }
 
-if (!$user){
+if (nkHasVisitor()){
     $theuser = include("themes/Impact_Nk/blocks/login.php");
 }
 else{
