6eb14a6068a285870a8495b49a562a3d7895c250
Samoth <samoth93@gmail.com>
2013-09-02 01:38:29 +0200

    Add new 404 page for administration

--- modules/404/index.php
+++ modules/404/index.php
@@ -9,32 +9,46 @@
 //-------------------------------------------------------------------------//
 defined('INDEX_CHECK') or die ('You can\'t run this file alone.');
 
-global $nuked, $language;
-translate('modules/Search/lang/' . $language . '.lang.php');
-translate('modules/404/lang/' . $language . '.lang.php');
+translate('modules/Search/lang/' . $GLOBALS['language'] . '.lang.php');
+translate('modules/404/lang/' . $GLOBALS['language'] . '.lang.php');
 
 if($_REQUEST['file'] == 'Admin' || $_REQUEST['page'] == 'admin'){
-	include('modules/Admin/design.php');
-	admintop();
-}else{
-	opentable();
+	require_once 'modules/Admin/views/layout.php';
+	adminHeader();
+?>
+    <div class="errorWrapper">
+        <span class="errorNum">404</span>
+        <div class="errorContent">
+            <span class="errorDesc"><span class="nkIcons icon-warning"></span><span><?php echo PAGE_NOT_FOUND; ?></span>
+            <div class="searchLine first">
+                <form action="">
+                    <input type="text" name="search" class="ac" placeholder="<?php echo SEARCH.' ...'; ?>" />
+                    <button type="submit" name="find" value=""><span class="icos-search"></span></button>
+                </form>
+            </div>
+            <div class="fluid">
+                <a href="index.php?file=Admin" title="" class="buttonM bLightBlue grid6"><?php echo BACK_DASHBOARD; ?></a>
+                <a href="index.php" title="" class="buttonM bRed grid6"><?php echo BACK_WEBSITE; ?></a>
+            </div>
+        </div>
+    </div>
+<?php
+
+    adminFooter();
 }
+else {
+	opentable();
 
-$error_title = ($_REQUEST['op'] != 'sql') ? '<big><b>' . $nuked['name'] . '</b></big><br /><br />' . _NOEXIST . '<br /><br />' : _ERROR404SQL . '<br /><br />';
+    $error_title = ($_REQUEST['op'] != 'sql') ? '<big><b>' . $nuked['name'] . '</b></big><br /><br />' . _NOEXIST . '<br /><br />' : _ERROR404SQL . '<br /><br />';
 
-echo '<div style="text-align: center; padding: 0 10px">' . $error_title . '
-          <form method="post" action="index.php?file=Search&amp;op=mod_search">
-              <p><input type="hidden" name="module" value="" /><input type="text" name="main" size="25" /></p>
-              <p><input type="submit" class="button" name="submit" value="' . _SEARCHFOR . '" /></p>
-              <p><a href="index.php?file=Search"><b>' . _ADVANCEDSEARCH . '</b></a> - <a href="javascript:history.back()"><b>' . _BACK . '</b></a></p>
-		  </form>
-	  </div>';
-
-if($_REQUEST['file'] == 'Admin' || $_REQUEST['page'] == 'admin'){
-	include('modules/Admin/design.php');
-	adminfoot();
-}else{
-	closetable();
+    echo '<div style="text-align: center; padding: 0 10px">' . $error_title . '
+              <form method="post" action="index.php?file=Search&amp;op=mod_search">
+                  <p><input type="hidden" name="module" value="" /><input type="text" name="main" size="25" /></p>
+                  <p><input type="submit" class="button" name="submit" value="' . _SEARCHFOR . '" /></p>
+                  <p><a href="index.php?file=Search"><b>' . _ADVANCEDSEARCH . '</b></a> - <a href="javascript:history.back()"><b>' . _BACK . '</b></a></p>
+    		  </form>
+    	  </div>';
+    closetable();
 }
 
 ?>
